d38e08345e8f512ec7e229af1646d4a904869ed8
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-03 12:07:07 +0200

    Start

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
