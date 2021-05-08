7c2ba547f5bea173e32fd2a71dcf21932646b1ea
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-08 11:10:34 +0200

    push 23

--- Assets/Scripts/Spawners/PlayerSpawner.cs
+++ Assets/Scripts/Spawners/PlayerSpawner.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     public GameObject player;
-    public GameObject mainCamera;
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, transform.rotation);
-        Instantiate(mainCamera, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
