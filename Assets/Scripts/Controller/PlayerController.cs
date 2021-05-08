0b57d7153a181737eae9a0cf6db9e83930010d20
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-08 11:10:13 +0200

    thinking

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,24 +7,49 @@ public class PlayerController : MonoBehaviour
     public GameObject projectilePrefab;
     private Vector3 projectileOffset;
     private Rigidbody playerRb;
-    private float speed = 500;
-    private GameObject focalPoint;
+    private float speed = 30f;
+    public CharacterController controller;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //float verticalInput = Input.GetAxis("Vertical");
+       // playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+      /*  if (Input.GetKeyDown(KeyCode.Space)) 
         {
+            projectileOffset = new Vector3(0, 1, 0);
             Instantiate(projectilePrefab, transform.position + projectileOffset, projectilePrefab.transform.rotation);
         }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a")) 
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("w")) 
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s")) 
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }*/
     }
 }
