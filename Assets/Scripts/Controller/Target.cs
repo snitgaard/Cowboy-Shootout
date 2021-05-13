10e5b54e6d7e7d334c23d1e1bbfc48e9e4a0dd42
The Joker <54842770+Nbruunj@users.noreply.github.com>
2021-05-13 11:47:49 +0200

    blod

--- Assets/Scripts/Controller/Target.cs
+++ Assets/Scripts/Controller/Target.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 20f;
+    public ParticleSystem effect;
   //  public ParticleSystem hitEffect;
 
     public void TakeDamage(float amount) 
     {
-       // hitEffect.Play();
+        // hitEffect.Play();
+        effect.Play();
         health -= amount;
         if(health <= 0f) 
         {
+            
             Die();
         }
     }
