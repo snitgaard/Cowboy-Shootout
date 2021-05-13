6708ef3556548e7a3a1cf2e1ce31dccb64b6dc18
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-13 12:36:41 +0200

    power up spawner script

---
+++ Assets/Scripts/Spawners/PowerUpSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+    public GameObject powerUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(powerUp, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
