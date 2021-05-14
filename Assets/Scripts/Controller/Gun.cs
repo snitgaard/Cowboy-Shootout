820b4dc0ff5f728e46ef26423f917b6bd37ce7b9
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-14 10:16:17 +0200

    Reload

--- Assets/Scripts/Controller/Gun.cs
+++ Assets/Scripts/Controller/Gun.cs
@@ -10,20 +10,40 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public float impactForce = 30f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 5f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
 
     private float nextTimeToFire = 0f;
-    
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (currentAmmo == -1)
+        {
+            currentAmmo = maxAmmo;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0) 
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire) 
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -31,10 +51,27 @@ public class Gun : MonoBehaviour
         }
     }
 
+    IEnumerator Reload() 
+    {
+        isReloading = true;
+        Debug.Log("Reload");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        animator.SetBool("Reloading", false);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
     void Shoot() 
     {
         muzzleFlash.Play();
         RaycastHit hit;
+
+        currentAmmo--;
+
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
         {
             Debug.Log(hit.transform.name);
